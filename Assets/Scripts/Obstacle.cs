36cab1ba2bfdd6fbd2e82450d1057fc20942b644
Silaenn <deokeldisilaen@gmail.com>
2025-07-17 18:53:39 +0700

    add score and particle system

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -13,6 +13,7 @@ public class Obstacle : MonoBehaviour
     [SerializeField] float minSpeed = 50f;
     [SerializeField] float maxSpeed = 150f;
     [SerializeField] float maxSpinSpeed = 10f;
+    [SerializeField] GameObject explosionObstacleEffect;
     void Start()
     {
         float randomSize = Random.Range(minSize, maxSize);
@@ -30,7 +31,16 @@ public class Obstacle : MonoBehaviour
 
     void Update()
     {
-        
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Vector2 contactPoint = collision.GetContact(0).point; 
+        GameObject bounceEffect = Instantiate(explosionObstacleEffect, contactPoint, Quaternion.identity);
+
+        // Destroy the effect after 1 second
+        Destroy(bounceEffect, 1f);
     }
 }
 
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,21 +4,61 @@
 
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 
 public class PlayerController : MonoBehaviour
 {
+    float elapsedTime = 0f;
+    float score = 0f;
+    [SerializeField] float scoreMultiplier = 10f;
     [SerializeField] float thrustForce = 1f;
     [SerializeField] float maxSpeed = 5f;
     [SerializeField] GameObject boosterFlame;
+    [SerializeField] UIDocument uIDocument;
+    Label scoretext;
+    Label highScoreText;
+    [SerializeField] GameObject explosionEffect;
+    Button restartButton;
 
     Rigidbody2D rb;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        scoretext = uIDocument.rootVisualElement.Q<Label>("ScoreLabel");
+        highScoreText = uIDocument.rootVisualElement.Q<Label>("HighScoreLabel");
+        restartButton = uIDocument.rootVisualElement.Q<Button>("RestartButton");
+        restartButton.style.display = DisplayStyle.None;
+        highScoreText.style.display = DisplayStyle.None;
+        restartButton.clicked += ReloadScene;
     }
 
     void Update()
+    {
+        UpdateScore();
+        MovePlayer();
+    }
+
+    void UpdateScore()
+    {
+        elapsedTime += Time.deltaTime;
+        score = Mathf.FloorToInt(elapsedTime * scoreMultiplier);
+        Debug.Log("Score: " + score);
+        scoretext.text = "Score: " + score;
+
+
+        int currentHighScore = PlayerPrefs.GetInt("HighScore", 0);
+        if (score > currentHighScore)
+        {
+            PlayerPrefs.SetInt("HighScore", (int)score);
+            PlayerPrefs.Save();
+        }
+
+        highScoreText.text = "HighScore: " + PlayerPrefs.GetInt("HighScore");
+    }
+
+    void MovePlayer()
     {
         if (Mouse.current.leftButton.isPressed)
         {
@@ -50,6 +90,14 @@ public class PlayerController : MonoBehaviour
     void OnCollisionEnter2D(Collision2D collision)
     {
         Destroy(gameObject);
+        Instantiate(explosionEffect, transform.position, transform.rotation);
+        restartButton.style.display = DisplayStyle.Flex;
+        highScoreText.style.display = DisplayStyle.Flex;
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
 
